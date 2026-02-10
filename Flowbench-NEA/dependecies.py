d7215af9b7a078c2ba81e30eeb64b42dcf9b7c70
kyled <k.pilkington@harrissutton.org.uk>
2026-02-10 13:47:54 +0000

    Added first iteration of GUI

--- Flowbench-NEA/dependecies.py
+++ Flowbench-NEA/dependecies.py
@@ -1,6 +1,8 @@
 import random
 import numpy
+import time
 import matplotlib.pyplot as plt
 import pandas as pd
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PySide6.QtGui import QScreen
 import sys
