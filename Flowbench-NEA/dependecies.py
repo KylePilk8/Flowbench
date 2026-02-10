67fbdcfe92e5cad0ca661bd3014d6d7c399bfb0c
KylePilk8 <k.pilkington@harrissutton.org.uk>
2026-02-10 10:37:46 +0000

    Add files via upload
    Added initial files

---
+++ Flowbench-NEA/dependecies.py
@@ -0,0 +1,6 @@
+import random
+import numpy
+import matplotlib.pyplot as plt
+import pandas as pd
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+import sys
---
+++ Flowbench-NEA/maingui.py
@@ -0,0 +1,5 @@
+from dependecies import *
+app = QApplication(sys.argv)
+window = QWidget()
+window.show()
+app.exec()
